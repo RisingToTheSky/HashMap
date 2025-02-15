4857ca96000327d5598f8076a483c7a735912029
RisingToTheSky <141631716+RisingToTheSky@users.noreply.github.com>
2025-02-15 22:11:41 +0100

    Fix set, get and has functions for keys

--- src/HashMap.js
+++ src/HashMap.js
@@ -1,15 +1,20 @@
+import { LinkedList } from "./LinkedList";
+import { Node } from "./Node";
 class HashMap {
-    constructor (loadFactor, capacity) {
-        this.loadFactor = loadFactor;
-        this.capacity = capacity;
-        this.array = [];
-        for (let i = 0; i < capacity * loadFactor; i++) {
-            this.array[i] = -1;
+    constructor () {
+        this.loadFactor = 0.75;
+        this.capacity = 16;
+        this.buckets = new Array(this.capacity);
+    }   
+
+    checkIndex(index) {
+        if (index < 0 || index >= this.buckets.length) {
+            throw new Error("Trying to access index out of bounds");
         }
     }
 
     hash(key) {
-        let hashCode = 19;
+        let hashCode = 0;
             
         const primeNumber = 31;
         for (let i = 0; i < key.length; i++) {
@@ -22,52 +27,99 @@ class HashMap {
 
     set(key, value) {
         let hashedKey = this.hash(key);
-        this.array[hashedKey] = value;
-        console.log(this.array);
-        console.log(hashedKey + " " +  value);
+        let bucket = this.buckets[hashedKey];
+        
+        // If the bucket exists
+        if (bucket) {
+            /*
+            If the bucket's head does not have  a key, make a node.
+            Else, change the value of the key.
+            */
+            if (!bucket.head.key) {
+                bucket = new Node(key, value);
+            } else if (bucket.head.key !== key) {
+                bucket.append(key, value);
+            } else {
+                bucket.head.value = value;
+            }
+
+        } else {
+            bucket = new LinkedList(key);
+            bucket.prepend(key, value);
+            this.buckets[hashedKey] = bucket;
+        }
+        
+        console.log(key + " " + hashedKey + " " +  value);
     }
 
     get(key) {
         let hashedKey = this.hash(key);
-        if (hashedKey < 0 || hashedKey >= this.array.length) {
-            throw new Error("Trying to access index out of bounds");
-        } else if (this.array[hashedKey] === -1) {
+        if (!this.buckets[hashedKey]) {
             return null;
         }
-        return this.array[hashedKey];
+
+        for (let bucket in this.buckets) {
+            if (this.buckets[bucket].head.key === key) {
+                return this.buckets[bucket].head.value;
+            } else {
+                let current = this.buckets[bucket].head;
+                while (current.nextNode) {
+                    if (current.nextNode.key === key) {
+                        return current.nextNode.value;
+                    }
+                    current = current.nextNode;
+                }
+            }
+        }
+
+        return null;
     }
 
     has(key) {
-        let hashedKey = this.hash(key);
-        if (hashedKey < 0 || hashedKey >= this.array.length) {
-            throw new Error("Trying to access index out of bounds");
-        } else if (this.array[hashedKey] === -1) {
-            return false;
+        for (let bucket in this.buckets) {
+            if (this.buckets[bucket].head.key === key) {
+                return true;
+            } else {
+                let current = this.buckets[bucket].head;
+                while (current.nextNode) {
+                    if (current.nextNode.key === key) {
+                        return true;
+                    }
+                    current = current.nextNode;
+                }
+            }
         }
-        return true;
+
+        return false;
     }
 
     remove(key) {
-        let hashedKey = this.hash(key);
-        if (hashedKey < 0 || hashedKey >= this.array.length) {
-            throw new Error("Trying to access index out of bounds");
-        } else if (this.array[hashedKey] === -1) {
-            return false;
+        for (let bucket in this.buckets) {
+            if (this.buckets[bucket].head.key === key) {
+                return true;
+            }
         }
-        console.log(this.array[hashedKey]);
-        this.array[hashedKey] = -1;
-        return true;
     }
 
     length() {
         let counter = 0;
-        for (let i = 0; i < this.array.length; i++) {
-            if (this.array[i] !== -1) {
+        for (let i = 0; i < this.buckets.length; i++) {
+            if (this.buckets[i] !== -1) {
                 counter++;
             }
         }
         
         return counter;
+    }
+
+    keys() {
+        let keyArray = [];
+
+        for (let key in this.buckets) {
+            keyArray.push(key);
+        }
+
+        return keyArray;
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,24 +1,30 @@
 import { HashMap } from "./HashMap";
 
-const test = new HashMap(0.75, 16);
+const test = new HashMap();
 
 test.set('apple', 'red');
 test.set('banana', 'yellow');
 test.set('carrot', 'orange');
 test.set('dog', 'brown');
 test.set('elephant', 'gray');
-// test.set('grape', 'purple');
-// test.set('hat', 'black');
-// test.set('ice cream', 'white');
-// test.set('jacket', 'blue');
-// test.set('kite', 'pink');
-// test.set('lion', 'golden');
+test.set('frog', 'green');
+test.set('grape', 'purple');
+test.set('hat', 'black');
+test.set('ice cream', 'white');
+test.set('jacket', 'blue');
+test.set('kite', 'pink');
+test.set('lion', 'golden');
 
 test.set('apple', 'green');
+console.log(test.get('hallo'));
 console.log(test.get('apple'));
+console.log(test.get('hat'));
 console.log(test.has('apple'));
-test.remove('apple');
-console.log(test.remove('ice cream'));
+console.log(test.has('hat'));
+console.log(test.has('hallo'));
+// test.remove('apple');
+// console.log(test.remove('ice cream'));
 console.log(test.length());
+console.log(test.keys());
 
 console.log(test);
